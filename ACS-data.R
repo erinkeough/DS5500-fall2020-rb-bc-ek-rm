f2b679f903626703f0e54d7acfb78e2d57e3569d
rossmarino <>
2020-10-02 21:32:31 -0400

    added simple visualizations

--- ACS-data.R
+++ ACS-data.R
@@ -1,12 +1,13 @@
 require(tidyverse)
 require(tidycensus)
+require(tmap)
 
 #### Use (install =TRUE) option to save the api key to R environment
 census_api_key("YOUR API KEY HERE", install = T)
 
 #### Loads a table of variables for the American Community Survey of a certain year
 vacs19<-load_variables(2019, "acs1/profile")
-vacs11<-load_variables(2011, "acs1/profile")
+vacs13<-load_variables(2013, "acs1/profile")
 
 bos_long<--71.057083
 bos_lat<-42.361145
@@ -39,6 +40,7 @@ bos_acs12_pop<-get_acs(
   state = 'MA',
   county = 'Suffolk', 
   year=2012,
+  geometry = T,
   survey = "acs5", 
   output = "wide"
 )
@@ -77,6 +79,7 @@ bos_acs18<-bos_acs18_pop%>%
 ## TODO: convert 2012 $ into 2018 $ (inflation)
 ##       meaningful names for variable
 ##       merge tables with variable for year
+##       convert columns to relevent percentages
 
 
 
@@ -84,11 +87,83 @@ bos_acs18<-bos_acs18_pop%>%
 #######   General EDA Visualization of census tracts
 ##########################################################################
 
-tm_shape(bos_acs19)+
-  tm_fill(col = percentEngHome,
-          palette = "Greens")
+### Smaller tables for visualization
+# Med HH income '18
+mhi18<-get_acs(
+  geography = "tract",
+  variables = "DP03_0062E",
+  state = 'MA',
+  county = 'Suffolk',
+  year=2018,
+  geometry = T,
+  survey = "acs5",
+  output = "wide"
+)
+
+tm_shape(mhi18)+
+  tm_fill(col = "DP03_0062E",
+          palette = "Greens",
+          style = 'jenks',
+          title = "Median HH\nIncome '18")+
+  tm_borders(col = "darkgray")
 
 
+# Med HH income '12
+mhi12<-get_acs(
+  geography = "tract",
+  variables = "DP03_0062E",
+  state = 'MA',
+  county = 'Suffolk',
+  year=2012,
+  geometry = T,
+  survey = "acs5",
+  output = "wide"
+)
+
+tm_shape(mhi12)+
+  tm_fill(col = "DP03_0062E",
+          palette = "Blues",
+          style = 'jenks',
+          title = "Median HH\nIncome '12")+
+  tm_borders(col = "darkgray")
+
+# Bach or higher Ed '18
+bach18<-get_acs(
+  geography = "tract",
+  variables = "DP02_0067E",
+  state = 'MA',
+  county = 'Suffolk',
+  year=2018,
+  geometry = T,
+  survey = "acs5",
+  output = "wide"
+)
+
+tm_shape(bach18)+
+  tm_fill(col = "DP02_0067E",
+          palette = "Greens",
+          style = 'jenks',
+          title = "Bach. or Higher\nEduc '18")+
+  tm_borders(col = "darkgray")
+
+# Bach or higher Ed '12
+bach12<-get_acs(
+  geography = "tract",
+  variables = c("DP02_0065E","DP02_0064E"),
+  state = 'MA',
+  county = 'Suffolk',
+  year=2012,
+  geometry = T,
+  survey = "acs5",
+  output = "wide"
+)
+bach12$bachUp<-bach12$DP02_0065E+bach12$DP02_0064E
+tm_shape(bach12)+
+  tm_fill(col = "bachUp",
+          palette = "Blues",
+          style = 'jenks',
+          title = "Bach. or Higher\nEduc '12")+
+  tm_borders(col = "darkgray")
 
 
 
