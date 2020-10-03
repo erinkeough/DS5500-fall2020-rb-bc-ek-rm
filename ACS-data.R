19a57aa15fdc7358327a137a2b5a1c9471e415dd
rossmarino <>
2020-10-02 22:15:35 -0400

    removed small visualizations, used main tables, fixed % eng at home

--- ACS-data.R
+++ ACS-data.R
@@ -19,7 +19,7 @@ bos_lat<-42.361145
 acs_vars<-c(#"B01001_001E",
             "DP03_0062E", "DP02_0059E","DP02_0060E","DP02_0061E","DP02_0062E", "DP02_0063E", "DP02_0067E",
             "DP05_0032E", "DP05_0033E","DP05_0034E", "DP05_0039E", "DP05_0047E", 
-            "DP02_0112E", "DP02_0113E", "DP02_0114E")
+            "DP02_0111PE", "DP02_0113E", "DP02_0114E")
 
 
 #######  call for data profiles  #########
@@ -48,7 +48,9 @@ bos_acs12_pop<-get_acs(
 bos_acs12<-bos_acs12%>%
   select(-NAME)%>%
   left_join(bos_acs12_pop, by = "GEOID")%>%
-  mutate(adjMHI = DP03_0062E*1.0935)
+  mutate(adjMHI = DP03_0062E*1.0935,
+         percEng = DP02_0111PE, 
+         percWhite = 100*DP05_0032E/B01001_001E)
 
 
 
@@ -76,13 +78,13 @@ bos_acs18_pop<-get_acs(
 
 bos_acs18<-bos_acs18%>%
   select(-NAME)%>%
-  left_join(bos_acs18_pop, by = "GEOID")
+  left_join(bos_acs18_pop, by = "GEOID")%>%
+  mutate(percEng = DP02_0111PE, percWhite = 100*DP05_0032E/B01001_001E)
 
 ################################
 ## TODO: convert 2012 $ into 2018 $ (inflation)
 ##       meaningful names for variable
 ##       merge tables with variable for year
-##       convert columns to relevent percentages
 
 
 
@@ -90,6 +92,7 @@ bos_acs18<-bos_acs18%>%
 #######   General EDA Visualization of census tracts
 ##########################################################################
 
+##### Med HH Income (2018 dollars)
 tm_shape(bos_acs18)+
   tm_fill(col = "DP03_0062E",
           palette = "Greens",
@@ -105,85 +108,18 @@ tm_shape(bos_acs12)+
           title = "Median HH\nIncome '12")+
   tm_borders(col = "darkgray")
 
+############### Perc Eng at Home 
 
-
-### Smaller tables for visualization
-# Med HH income '18
-mhi18<-get_acs(
-  geography = "tract",
-  variables = "DP03_0062E",
-  state = 'MA',
-  county = 'Suffolk',
-  year=2018,
-  geometry = T,
-  survey = "acs5",
-  output = "wide"
-)
-
-tm_shape(mhi18)+
-  tm_fill(col = "DP03_0062E",
+tm_shape(bos_acs18)+
+  tm_fill(col = "percEng",
           palette = "Greens",
           style = 'jenks',
-          title = "Median HH\nIncome '18")+
+          title = "% Eng. Only HH '18")+
   tm_borders(col = "darkgray")
 
-
-# Med HH income '12
-mhi12<-get_acs(
-  geography = "tract",
-  variables = "DP03_0062E",
-  state = 'MA',
-  county = 'Suffolk',
-  year=2012,
-  geometry = T,
-  survey = "acs5",
-  output = "wide"
-)
-
-tm_shape(mhi12)+
-  tm_fill(col = "DP03_0062E",
+tm_shape(bos_acs12)+
+  tm_fill(col = "percEng",
           palette = "Blues",
           style = 'jenks',
-          title = "Median HH\nIncome '12")+
+          title = "% Eng. Only HH '12")+
   tm_borders(col = "darkgray")
-
-# Bach or higher Ed '18
-bach18<-get_acs(
-  geography = "tract",
-  variables = "DP02_0067E",
-  state = 'MA',
-  county = 'Suffolk',
-  year=2018,
-  geometry = T,
-  survey = "acs5",
-  output = "wide"
-)
-
-tm_shape(bach18)+
-  tm_fill(col = "DP02_0067E",
-          palette = "Greens",
-          style = 'jenks',
-          title = "Bach. or Higher\nEduc '18")+
-  tm_borders(col = "darkgray")
-
-# Bach or higher Ed '12
-bach12<-get_acs(
-  geography = "tract",
-  variables = c("DP02_0065E","DP02_0064E"),
-  state = 'MA',
-  county = 'Suffolk',
-  year=2012,
-  geometry = T,
-  survey = "acs5",
-  output = "wide"
-)
-bach12$bachUp<-bach12$DP02_0065E+bach12$DP02_0064E
-tm_shape(bach12)+
-  tm_fill(col = "bachUp",
-          palette = "Blues",
-          style = 'jenks',
-          title = "Bach. or Higher\nEduc '12")+
-  tm_borders(col = "darkgray")
-
-
-
