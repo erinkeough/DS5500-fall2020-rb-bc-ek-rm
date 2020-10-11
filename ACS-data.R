bbb6a5628397fb7cb7d702fad834849b974be46a
rossmarino <>
2020-10-11 15:41:04 -0400

    renamed variables, added csv for 2010 with readable vars

--- ACS-data.R
+++ ACS-data.R
@@ -17,9 +17,9 @@ bos_lat<-42.361145
 ######                       white, black, Nat. Am., asian, HI or PAI,
 ######                       lang at home eng only, lang at home other, lang other than eng and eng not spoken 'well')
 acs_vars<-c(#"B01001_001E",
-            "DP03_0062E", "DP02_0059E","DP02_0060E","DP02_0061E","DP02_0062E", "DP02_0063E", "DP02_0067E",
-            "DP05_0032E", "DP05_0033E","DP05_0034E", "DP05_0039E", "DP05_0047E", 
-            "DP02_0111PE", "DP02_0113E", "DP02_0114E")
+  "DP03_0062E", "DP02_0059E","DP02_0060E","DP02_0061E","DP02_0062E", "DP02_0063E",
+  "DP05_0032E", "DP05_0033E","DP05_0034E", "DP05_0039E", "DP05_0047E", 
+  "DP02_0111PE", "DP02_0113E", "DP02_0114E")
 
 
 #######  call for data profiles  #########
@@ -54,9 +54,22 @@ bos10<-bos_acs10%>%
   mutate(GEOID = str_remove(bos_acs10$GEOID, "25025"))%>%
   filter(!str_detect(GEOID, "^9"))
 
+### ( total pop, 
+####  median income, Ed. Attainment: <9th, 9th-12th, HS Dip., some col, assoc deg, bach or higher, 
+####  white, black, Nat. Am., asian, HI or PAI,
+##### lang at home eng only, lang at home other, lang other than eng and eng not spoken 'well')
+# acs_vars<-c(#"B01001_001E",
+#   "DP03_0062E", "DP02_0059E","DP02_0060E","DP02_0061E","DP02_0062E", "DP02_0063E",
+#   "DP05_0032E", "DP05_0033E","DP05_0034E", "DP05_0039E", "DP05_0047E", 
+#   "DP02_0111PE", "DP02_0113E", "DP02_0114E")
+bos10_trim<-bos10%>%
+  mutate(tot_pop = B01001_001E, 
+         med_income = DP03_0062E, ed_hs = DP02_0059E+DP02_0060E+DP02_0061E,
+         rc_white =DP05_0032E, lang_eng = DP02_0111PE)%>%
+  select(tot_pop, med_income, ed_hs, rc_white, lang_eng, percEng, percWhite)
+  
 
-
-#write_csv(bos_acs10, "bos_2010.csv")
+write_csv(bos10_trim, "census-csv/bos_2010_trim.csv")
 
 ##### getting 2019 data
 bos_acs18<-get_acs(
