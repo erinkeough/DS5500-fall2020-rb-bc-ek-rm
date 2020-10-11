d65f57e18b3513e4124fdf5b850f997d640884a9
rossmarino <>
2020-10-11 12:42:44 -0400

    scraping tract-level data 2000

---
+++ bostract00.R
@@ -0,0 +1,23 @@
+##     2000 Census data by Tract of Suffolk county MA (Boston, ma)
+##
+## Scraping from FFIEC website (both pages)
+bos001<-read_html("https://www.ffiec.gov/census/report.aspx?year=2000&county=025&tract=ALL&state=25&report=demographic")%>%
+  html_node("form table")%>%
+  html_table()
+bos002<-read_html("https://www.ffiec.gov/census/report.aspx?year=2000&county=025&tract=ALL&state=25&report=demographic&page=2")%>%
+  html_node("form table")%>%
+  html_table()
+bostract00<-rbind(bos001, bos002)
+
+### Clean up names and 
+bostract00<-bostract00%>%
+  rename(tract = `Tract Code`, med_income00 = `2000 Est. Tract/ BNA Median Family Income`,
+         med_income90 = `1990 Tract/ BNA Median Family Income`, tot_pop = `Tract/ BNA Population`,
+         nonwhite_pop = `Minority Population`, owner_occ = `Owner Occupied Units`)%>%
+  select(tract, med_income00, med_income90, tot_pop, nonwhite_pop, owner_occ)%>%
+  mutate(GEOID = as.integer(tract*100), med_income00 = str_remove_all(med_income00, "[\\$\\,]"),
+         med_income90 = str_remove_all(med_income90, "[\\$\\,]"))
+rm(bos001, bos002)
+
+
+#write_csv(bostract00, "bos_2000.csv")
